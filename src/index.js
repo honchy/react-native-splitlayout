e32e642ac50efdab697defb23a6d665cd98debcb
ghq甘红旗 <hqgan@ctrip.com>
2016-11-08 22:17:46 +0800

    first blood

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { View, Dimensions } from 'react-native'
+
+let _winsize = Dimensions.get('window'),
+    vw = _winsize.width / 100,
+    vh = _winsize.height / 100
+
+export default class SplitLayout extends Component {
+
+    render() {
+
+        let { style=[], width, height, flex, split, ...others } = this.props
+
+        let widthNum = parseFloat(width, 10)
+        let props = [width, height]
+        // process width
+        props.forEach((val, index) => {
+            let prop = index === 0 ? 'width' : 'height'
+
+            numVal = parseFloat(val, 10)
+            numVal = /vw$/.test(val) ? numVal * vw : numVal
+            numVal = /vh$/.test(val) ? numVal * vh : numVal
+
+            if(numVal) {
+                // default added put first , it'll be overwrite then
+                style.unshift({
+                    flex: 0
+                })
+                // specified style can't be overwirtted
+                style.push({
+                    [prop]: numVal
+                })
+            }
+        })
+
+        // process flex
+        if(flex) {
+            style.push({
+                flex: flex
+            })
+        }
+        // process split
+        if(split) {
+            style.push({
+                flexDirection: split === '|' ? 'row' : 'column'
+            })
+
+            style.unshift({
+                alignItems: 'stretch',
+                justifyContent: 'center'
+            })
+        }
+
+        return (
+            <View style={ style } { ...others }>
+                { this.props.children }
+            </View>
+        )
+    }
+}
